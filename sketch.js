5ed5338a0b6d6ecefe3ae2798fd5eec854589946
Erin Flint <42758837+erinflint@users.noreply.github.com>
2019-10-28 18:48:53 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  textSize(32);
+ text('Hi, I am an iteration of Erin!', 10, 30);
+ square(150,64,80);
+ circle(175,100,20);
+ circle(205,100,20);
+ line(185,100,195,100);
+ triangle(185,110,190,105,195,110);
+ rect(175,115,30,15);
+ triangle(150,230,190,145,230,230);
+ line(200,230,200,270);
+ line(180,230,180,270);
+ line(130,180,168,190);
+ line(160,180,198,190);
+ point(200,100);
+ point(180,100);
+ textSize(10);
+ text('Erin Flint',10,390);
+}
